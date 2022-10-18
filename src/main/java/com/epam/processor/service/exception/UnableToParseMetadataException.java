2b797ae8ca9d341f590cd667693b772d9a179984
Nadzeya_Chybisava <nadezhdalobanok@gmail.com>
2022-10-18 13:57:53 +0300

    Add work with RabbitMQ

---
+++ src/main/java/com/epam/processor/service/exception/UnableToParseMetadataException.java
@@ -0,0 +1,8 @@
+package com.epam.processor.service.exception;
+
+public class UnableToParseMetadataException extends RuntimeException {
+
+    public UnableToParseMetadataException() {
+        super();
+    }
+}
